b3b6efceb1739bf723724811da6a08db5c8f3063
Ștefan Cenușă <cenusa.s@gmail.com>
2015-12-09 22:31:53 +0200

    java

---
+++ src/Main.java
@@ -0,0 +1,42 @@
+import org.apache.pdfbox.cos.COSDocument;
+import org.apache.pdfbox.pdfparser.PDFParser;
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.util.PDFTextStripper;
+
+import java.io.File;
+import java.io.FileInputStream;
+
+public class Main {
+
+    public static void main(String args[]) {
+
+        PDFParser parser = null;
+        PDDocument pdDoc = null;
+        COSDocument cosDoc = null;
+        PDFTextStripper pdfStripper;
+
+        String parsedText;
+        String fileName = args[0];
+        File file = new File(fileName);
+        try {
+            parser = new PDFParser(new FileInputStream(file));
+            parser.parse();
+            cosDoc = parser.getDocument();
+            pdfStripper = new PDFTextStripper();
+            pdDoc = new PDDocument(cosDoc);
+            parsedText = pdfStripper.getText(pdDoc);
+            System.out.println(parsedText.replaceAll("[/^(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)*\/?$/]", ""));
+        } catch (Exception e) {
+            e.printStackTrace();
+            try {
+                if (cosDoc != null)
+                    cosDoc.close();
+                if (pdDoc != null)
+                    pdDoc.close();
+            } catch (Exception e1) {
+                e.printStackTrace();
+            }
+
+        }
+    }
+}
