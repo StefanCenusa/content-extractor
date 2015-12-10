54f5ee236ae174776d6707eec696f8aa979c1135
Andrei Cioromila <andrei.cioromila@thinslices.com>
2015-12-10 10:33:52 +0200

    Changed output from console to file
    The app takes a second parameter as the output file name. If it is not specified, it will use "output.txt"

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,9 @@ import org.apache.pdfbox.util.PDFTextStripper;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class Main {
 
@@ -17,6 +20,12 @@ public class Main {
 
         String parsedText;
         String fileName = args[0];
+        String outputFile;
+        if (args.length == 2) {
+             outputFile = args[1];
+        } else {
+            outputFile = "output.txt";
+        }
         File file = new File(fileName);
         try {
             parser = new PDFParser(new FileInputStream(file));
@@ -25,7 +34,9 @@ public class Main {
             pdfStripper = new PDFTextStripper();
             pdDoc = new PDDocument(cosDoc);
             parsedText = pdfStripper.getText(pdDoc);
-            System.out.println(parsedText.replaceAll("@(https?|ftp)://(-\\.)?([^\\s/?\\.#-]+\\.?)+(/[^\\s]*)?$@iS", ""));
+            parsedText = parsedText.replaceAll("@(https?|ftp)://(-\\.)?([^\\s/?\\.#-]+\\.?)+(/[^\\s]*)?$@iS", "");
+            writeToFile(outputFile, parsedText);
+//            System.out.println(parsedText);
         } catch (Exception e) {
             e.printStackTrace();
             try {
@@ -39,4 +50,25 @@ public class Main {
 
         }
     }
+
+    public static void writeToFile(String fileName, String content) {
+        try {
+
+            File file = new File("./" + fileName);
+            if (!file.exists()) {
+                file.createNewFile();
+            }
+
+            FileWriter fw = new FileWriter(file.getAbsoluteFile());
+            BufferedWriter bw = new BufferedWriter(fw);
+            bw.write(content);
+            bw.close();
+
+            System.out.println("Done");
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
+
